4a1367f9b0b6cf012596b3c6bfcd8dc25782be49
Matthias Hörmann <mhoermann@gmail.com>
2026-05-03 13:02:42 +0200

    [gentoo-patchset] 0005-musl-Only-use-system-heap-reporter-with-glibc.patch

--- xpcom/base/nsMemoryReporterManager.cpp
+++ xpcom/base/nsMemoryReporterManager.cpp
@@ -698,6 +698,7 @@ static bool InSharedRegion(mach_vm_address_t aAddr, cpu_type_t aType) {
   return NS_OK;
 }
 
+#ifdef __GLIBC__
 #  define HAVE_SYSTEM_HEAP_REPORTER 1
 // Windows can have multiple separate heaps, but we should not touch non-default
 // heaps because they may be destroyed at anytime while we hold a handle.  So we
@@ -730,6 +731,7 @@ static bool InSharedRegion(mach_vm_address_t aAddr, cpu_type_t aType) {
   *aSizeOut = heapSize;
   return NS_OK;
 }
+#endif
 
 struct SegmentKind {
   DWORD mState;
